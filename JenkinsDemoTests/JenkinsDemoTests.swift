bc4650527d009f7f2935c7aa08e7a0c0c475189f
Matt Glover <matt@duchysoftware.com>
2019-02-10 23:56:57 +0000

    Added testSimpleMultiplication test

--- JenkinsDemoTests/JenkinsDemoTests.swift
+++ JenkinsDemoTests/JenkinsDemoTests.swift
@@ -6,4 +6,8 @@ class JenkinsDemoTests: XCTestCase {
     func testExample() {
         XCTAssertEqual(1, 1)
     }
+
+    func testSimpleMultiplication() {
+        XCTAssertEqual(5 * 6, 30)
+    }
 }
